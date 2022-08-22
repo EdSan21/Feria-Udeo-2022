1351cf273e94916a89d4cf84485c39f7ccdab4ed
Emanuel <emanuelcf@outlook.es>
2022-08-21 22:43:23 -0600

    Cambio Typescript podio

--- feriaUdeo2022/src/app/components/premiacion/sub/podio/podio.component.ts
+++ feriaUdeo2022/src/app/components/premiacion/sub/podio/podio.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PodioComponent implements OnInit {
 
-  constructor() { }
+  
+
+  Podio: any[3] = new Array(3);
+
+
+  constructor() {
+
+    this.Podio[0] = {
+      Titulo: "Circuito de carreras volador automatizado ecologico",
+      Categoria:"Mecatronica",
+      Imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Nao_Robot_%28Robocup_2016%29.jpg/800px-Nao_Robot_%28Robocup_2016%29.jpg"
+    } 
+    this.Podio[1] = {
+      Titulo: "Circuito digital",
+      Categoria:"Programacion",
+      Imagen:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Nao_Robot_%28Robocup_2016%29.jpg/800px-Nao_Robot_%28Robocup_2016%29.jpg"
+    } 
+    this.Podio[2] = {
+      Titulo: "Maquina de Goldenberg",
+      Categoria:"Unica",
+      Imagen:"https://m.media-amazon.com/images/I/71g1TNP7JXL._AC_SX425_.jpg"
+    } 
+
+    console.log(this.Podio)
+    
+    
+   }
 
   ngOnInit(): void {
+    
+    
   }
 
 }
