08aab5f607dff7ff4948f3f0bf3b0c495fc3e017
Emanuel <emanuelcf@outlook.es>
2022-09-17 17:28:19 -0600

    0.2

--- feriaUdeo2022/src/app/services/RutasApi.ts
+++ feriaUdeo2022/src/app/services/RutasApi.ts
@@ -1,4 +1,4 @@
 export class Rutas{
     public static RutaDatos='http://emacont-001-site2.btempurl.com/api/';
-    public static RutaVotos='https://localhost:4430/api/';
+    public static RutaVotos='http://emacont-001-site3.btempurl.com/api/';
 }
